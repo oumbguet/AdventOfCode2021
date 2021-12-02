042ceadc61baef2d5edda868ba7adc2ec0fa0384
florian millet <florian.millet@epitech.eu>
2021-12-02 08:49:45 +0100

    change var names

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,18 +1,11 @@
 file = File.open("input")
 
-file_data = file.readlines.map(&:chomp).map(&:to_i)
-
-arr = []
-
-0.upto(file_data.length - 2) do |i|
-  arr.push file_data[i..(i + 2)].reduce(:+)
-end
+data = file.readlines.map(&:chomp).map(&:to_i)
 
 count = 0
 
-puts arr[0]
 1.upto(arr.length - 2) do |i|
-  count += (arr[i] > arr[i - 1] ? 1 : 0)
+  count += (data[i..(i + 2)].reduce(:+) > data[(i - 1)..(i + 1)].reduce(:+) ? 1 : 0)
 end
 
 puts count
