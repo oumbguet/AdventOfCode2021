693257a510882ababaaa4c7b35ebed41f91da2d8
Florian Millet <florian.mil68@gmail.com>
2021-12-08 10:20:22 +0100

    Update part2.rb

--- day08/part2.rb
+++ day08/part2.rb
@@ -2,14 +2,14 @@ file = File.open("input", "r")
 data = file.readlines.map(&:chomp)
 
 class Line
-  attr_accessor :digits, :outputs, :sides
+  attr_accessor :digits, :outputs, :set1, :set4, :set7, :set8, :setTable
 
   def initialize(line)
     splitted = line.split(" |")
 
     @digits = splitted[0].split(" ")
     @outputs = splitted[1].split(" ")
-    @sides = Array.new(7) {''}
+    @setTable = {}
   end
 
   def dump
@@ -25,35 +25,60 @@ class Line
     return nil
   end
 
-  def set_sides
-    var1 = get_set_by_length(2)
-    var4 = get_set_by_length(4)
-    var7 = get_set_by_length(3)
-    var8 = get_set_by_length(7)
+  def def_sets
+    @set1 = get_set_by_length(2)
+    @set4 = get_set_by_length(4)
+    @set7 = get_set_by_length(3)
+    @set8 = get_set_by_length(7)
+  end
 
-    a = var7 - var1
-    bd = var4 - var1
-    cf = var1
-    eg = var8 - var7 - var4
+  def resolve_sides
+    @digits.each do |digit|
+        if digit.length == 2
+            setTable[digit.split('').to_set] = 1
+        elsif digit.length == 4
+            setTable[digit.split('').to_set] = 4
+        elsif digit.length == 3
+            setTable[digit.split('').to_set] = 7
+        elsif digit.length == 7
+            setTable[digit.split('').to_set] = 8
+        elsif digit.length == 5
+            if (@set1 & digit.split('').to_set).size == 2
+                setTable[digit.split('').to_set] = 3
+            elsif (@set4 & digit.split('').to_set).size == 2
+                setTable[digit.split('').to_set] = 2
+            else
+                setTable[digit.split('').to_set] = 5
+            end
+        elsif digit.length == 6
+            if (@set1 & digit.split('').to_set).size == 1
+                setTable[digit.split('').to_set] = 6
+            elsif (@set4 & digit.split('').to_set).size == 4
+                setTable[digit.split('').to_set] = 9
+            else
+                setTable[digit.split('').to_set] = 0
+            end
+        end
+    end
+  end
 
-
-    puts a, bd, cf, eg
+  def get_solutions
+    @solutions = []
+    @outputs.each do |output|
+        @solutions << setTable[output.split('').to_set]
+    end
+    @solutions
   end
 
 end
 
 lines = data.map { |line| Line.new(line) }
 
+count = 0
+lines.each do |line|
+    line.def_sets
+    line.resolve_sides
+    count += line.get_solutions.join('').to_i
+end
 
-
-lines[0].set_sides()
-# lines[0].dump()
-
-
-# C   4
-# F   4
-# B   2
-# D   2
-# A   2
-# E   1
-# G   1
+puts count
